eb81606ad92e0c907e937fe2d0afa5830fe49c13
Vramanouski <119852812+Vramanouski@users.noreply.github.com>
2023-02-06 17:29:09 +0300

    add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let ans = 0
+    }
 
     setRange(min, max) {
-
+       this.minimum = min
+       this.maximum = max
     }
 
     guess() {
-
+        this.ans = Math.round((this.maximum+this.minimum)/2)
+        return this.ans
     }
 
     lower() {
-
+        this.maximum = this.ans
+        return this.maximum
     }
 
     greater() {
+        this.minimum = this.ans
+        return this.minimum
 
     }
 }
